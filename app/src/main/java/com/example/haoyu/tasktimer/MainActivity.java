c05f41146976d2bc9abe184c0135d2828c9d10a4
HAOYU WU <Haoyu@MyMac.local>
2017-01-10 17:41:33 -0800

    TaskTimer_First commit

---
+++ app/src/main/java/com/example/haoyu/tasktimer/MainActivity.java
@@ -0,0 +1,127 @@
+package com.example.haoyu.tasktimer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity implements CursorRecyclerViewAdapter.OnTaskClickListener,
+            AddEditActivityFragment.OnSaveClicked {
+
+    private static final String TAG = "MainActivity";
+
+    // Whether or not thje activity is in 2-pane mode
+    // i.e. running in landscape on a tablet
+    private boolean mTwoPane = false;
+
+    private static final String ADD_EDIT_FRAGMENT = "AddEditFragment";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (findViewById(R.id.task_details_container) != null) {
+            // The detail container view will be present only in the large-screen layouts (res/values-land and res/values-sw600dp).
+            // If this view is present, then the activity should be in two-pane mode.
+            mTwoPane = true;
+        }
+    }
+
+    @Override
+    public void onSaveClicked() {
+        Log.d(TAG, "onSaveClicked: starts");
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.task_details_container);
+        if (fragment != null) {
+//            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//            fragmentTransaction.remove(fragment);
+//            fragmentTransaction.commit();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .remove(fragment)
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (id) {
+            case R.id.menumain_addTask:
+                taskEditRequest(null);
+                break;
+            case R.id.menumain_showDurations:
+                break;
+            case R.id.menumain_settings:
+                break;
+            case R.id.menumain_showAbout:
+                break;
+            case R.id.menumain_generate:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onEditClick(Task task) {
+        taskEditRequest(task);
+    }
+
+    @Override
+    public void onDeleteClick(Task task) {
+        getContentResolver().delete(TasksContract.buildTaskUri(task.getId()), null, null);
+    }
+
+    private void taskEditRequest(Task task) {
+        Log.d(TAG, "taskEditRequest: starts");
+        if (mTwoPane) {
+            Log.d(TAG, "taskEditRequest: in two-pane mode (tablet)");
+            AddEditActivityFragment fragment = new AddEditActivityFragment();
+
+            Bundle arguments = new Bundle();
+            arguments.putSerializable(Task.class.getSimpleName(), task);
+            fragment.setArguments(arguments);
+
+//            FragmentManager fragmentManager = getSupportFragmentManager();
+//            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//            fragmentTransaction.replace(R.id.task_details_container, fragment);
+//            fragmentTransaction.commit();
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.task_details_container, fragment)
+                    .commit();
+
+        } else {
+            Log.d(TAG, "taskEditRequest: in single-pane mode (phone)");
+            // in single-pane mode, start the detail activity for the selected item Id.
+            Intent detailIntent = new Intent(this, AddEditActivity.class);
+            if (task != null) { // editing a task
+                detailIntent.putExtra(Task.class.getSimpleName(), task);
+                startActivity(detailIntent);
+            } else { // adding a new task
+                startActivity(detailIntent);
+            }
+        }
+    }
+}
